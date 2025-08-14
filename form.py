532449d06a05cd0295929ed6d0764e407f1e54ab
iceman-twitch <iceman.twitch.contact@gmail.com>
2025-08-14 15:42:37 +0200

    update for login save.

--- form.py
+++ form.py
@@ -1,6 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox
 from teveclub import teveclub
+from icon import icon
+import json
+import os
+
+CREDENTIALS_FILE = "credentials.json"
+
+def load_credentials():
+    """Load saved credentials from JSON file if it exists"""
+    if os.path.exists(CREDENTIALS_FILE):
+        try:
+            with open(CREDENTIALS_FILE, 'r') as f:
+                return json.load(f)
+        except (json.JSONDecodeError, PermissionError):
+            return None
+    return None
+
+def save_credentials(username, password):
+    """Save credentials to JSON file"""
+    try:
+        with open(CREDENTIALS_FILE, 'w') as f:
+            json.dump({"username": username, "password": password}, f)
+        return True
+    except (PermissionError, TypeError):
+        return False
 
 def on_start_click():
     username = username_entry.get()
@@ -28,6 +52,10 @@ def on_start_click():
             try:
                 teve.Guess()
                 messagebox.showinfo("Success", "Your pet done all works for today!!")
+                if save_credentials(username, password):
+                    pass
+                else:
+                    messagebox.showerror("Error", "Could not save credentials")
             except:
                 messagebox.showinfo("Failed", "Guess Game failed!!")
         else:
@@ -37,6 +65,7 @@ def on_start_click():
 root = tk.Tk()
 root.title("Teveclub")
 root.geometry("300x200")
+root.resizable(False, False)  # <--- THIS LINE PREVENTS RESIZING
 
 # Set window icon (replace 'icon.ico' with your actual icon file)
 try:
@@ -50,19 +79,26 @@ except:
     except:
         print("Could not load icon file")
 
+# Try to load saved credentials
+credentials = load_credentials()
+
 # Username Label and Entry
 username_label = tk.Label(root, text="Username:")
 username_label.pack(pady=(20, 0))
 
 username_entry = tk.Entry(root, width=30)
 username_entry.pack(pady=5)
-
+if credentials:
+    username_entry.insert(0, credentials.get("username", ""))
+    
 # Password Label and Entry
 password_label = tk.Label(root, text="Password:")
 password_label.pack()
 
 password_entry = tk.Entry(root, width=30, show="*")  # show="*" makes it display asterisks
 password_entry.pack(pady=5)
+if credentials:
+    password_entry.insert(0, credentials.get("password", ""))
 
 # Start Button
 start_button = tk.Button(root, text="Start", command=on_start_click)
---
+++ icon.py
@@ -0,0 +1,8 @@
+import base64
+
+class icon:
+    def Get(self):
+        ICON__BASE64 = """
+                 (     (                    H   H          ÿÿÿ ¹½¾ ]ˆ Qz /q Ÿ¬ ÷óï üù÷ ¦·À p¡ ïìï ÔÕ× }¶ ŒÊ ÷ÿÿ ŠÇ ""3Eg  x“2I2§  »C#V   SWƒVp  ¡ƒ&&p  ¡B#Jw  rLÍ”ÂŽ  ÍÝÝÜÁà ÍÝÝÝô‡
+            MÜÜMÔ™˜ ¹ÝÈ´ÙÜÂ rýJ™Ü• œJIX ™ ´<Åº  µ eŸÃW    $*§                                                                """
+        return ICON_BASE64
